7a9bb5bb7e721eac7775d32380118d5f0df34d12
Carlos Andres Rivera <cr4185320@gmail.com>
2022-09-18 04:29:32 -0500

    Updated libpath and warnings

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 pushd bin
 
-CL -W4 -wd4053 -wd4101 -wd4100 -wd4189 -wd4201 -wd4996 -nologo -FC -Zi -MDd -I ..\deps\glad\include -I ..\deps\glfw\include -I ..\deps\HMM -I ..\deps\stb ..\src\tetris.c ..\deps\glad\src\glad.c -link -LIBPATH:C:\Dev\game2D\deps\glfw\bin\x64\lib-vc2019 user32.lib shell32.lib gdi32.lib opengl32.lib glfw3.lib
+CL -W4 -wd4053 -wd4101 -wd4100 -wd4189 -wd4201 -wd4996 -nologo -FC -Zi -MDd -I ..\deps\glad\include -I ..\deps\glfw\include -I ..\deps\HMM -I ..\deps\stb ..\src\tetris.c ..\deps\glad\src\glad.c -link -LIBPATH:C:\Dev\tetris\deps\glfw\bin\x64\lib-vc2019 user32.lib shell32.lib gdi32.lib opengl32.lib glfw3.lib
 
 popd
